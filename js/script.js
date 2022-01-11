907d90b3f0427259221573b792a60e0f2e51c420
dvenomb98 <91594441+dvenomb98@users.noreply.github.com>
2022-01-11 21:42:03 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let checkbox = document.querySelector("#myCheck")
+let cena = document.querySelectorAll(".cena")
+let slider = document.querySelector(".slider")
+
+
+
+
+console.log(cena)
+
+
+checkbox.addEventListener("change", function(event){
+
+    if (event.target.checked === true) {
+        cena[0].lastChild.nodeValue = "19.99"
+        cena[1].lastChild.nodeValue = "24.99"
+        cena[2].lastChild.nodeValue = "39.99"
+        slider.style.opacity = "0.5"
+
+    }
+    else if (event.target.checked === false) {
+        cena[0].lastChild.nodeValue = "199.99"
+        cena[1].lastChild.nodeValue = "249.99"
+        cena[2].lastChild.nodeValue = "399.99"
+        slider.style.opacity = "1"
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
